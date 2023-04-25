c843ac783e258ec2cda45c2abf3329e08f792cf3
Valentine N. Akponor <richboyflex15@gmail.com>
2023-04-25 17:55:07 +0000

    mysql

--- 0x14-mysql/3-new_user.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER IF NOT EXISTS 'replica_user'@'%' IDENTIFIED BY 'password';
-GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
-GRANT SELECT ON mysql.user to 'holberton_user'@'localhost';
--- 0x14-mysql/README.md
+++ 0x14-mysql/README.md
@@ -1,4 +1,1 @@
-# 0x14. MySQL
-
-## Resources
-- [MySQL primary replica setup](https://www.digitalocean.com/community/tutorials/how-to-set-up-replication-in-mysql)
+# Solutions to tasks on MySQL
